f83de87d52ab12d71a004bd785f49d540a8b0c5f
kimjunghun <lapulass@naver.com>
2021-07-24 18:32:45 +0900

    정헌 : aspdotnetcore test project

---
+++ ASPDOTNETCORE/ASPDOTNETCORE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASPDOTNETCORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ASPDOTNETCORE/README.md
@@ -0,0 +1,3 @@
+# C# ASP .NET CORE 기본 프레임워크 프로젝트
+
+* ASP .NET CORE를 사용하면서 자주 사용하는 기본 뼈대
