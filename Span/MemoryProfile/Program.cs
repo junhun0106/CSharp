439db9a67f2fc2ffb5a01003f36bb4739b785832
kimjunghun <lapulass@naver.com>
2021-08-26 20:20:00 +0900

    메모리 프로파일 하기 위한 테스트 프로젝트 추가

---
+++ Span/MemoryProfile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Collections.Generic;
+using ClassLibrary1;
+using System;
+
+namespace MemoryProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>(10000);
+            for (int i = 0; i < 10000; ++i) {
+                list.Add(i);
+            }
+
+            var sb = new StringBuilder();
+            sb.Append("{");
+            foreach (var i in list) {
+                if (sb.Length == 0) {
+                    sb.Append(i.ToString());
+                } else {
+                    sb.Append($"; {i}");
+                }
+            }
+            sb.Append("}");
+
+            var testString = sb.ToString();
+            for (int i = 1; i <= 1000; ++i) {
+                testString.ToStringArraySafe();
+                testString.AsSpan().ToStringArraySafe();
+                testString.AsSpan().ToStringArraySafe_2();
+                testString.AsSpan().ToStringArraySafe_Custom();
+
+                if (i % 100 == 0) {
+                    Console.WriteLine($"{i} test...");
+                }
+            }
+        }
+    }
+}
