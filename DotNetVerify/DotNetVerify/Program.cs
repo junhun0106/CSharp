d2570ca26706f52378ba4f79967f812f6266695d
kimjunghun <lapulass@naver.com>
2021-12-29 17:39:11 +0900

    사용하지 않는 코드들 삭제

--- DotNetVerify/DotNetVerify/Program.cs
+++ DotNetVerify/DotNetVerify/Program.cs
@@ -2,25 +2,6 @@
 global using System.Reflection;
 global using BenchmarkDotNet.Attributes;
 
-using BenchmarkDotNet.Columns;
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Diagnosers;
-using BenchmarkDotNet.Environments;
-using BenchmarkDotNet.Exporters;
-using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
-using BenchmarkDotNet.Validators;
-
-using DotNetVerify.SealedClass;
 
 BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-
-//var customConfig = ManualConfig
-//    .Create(DefaultConfig.Instance)
-//    .AddValidator(JitOptimizationsValidator.FailOnError)
-//    .AddDiagnoser(MemoryDiagnoser.Default)
-//    .AddColumn(StatisticColumn.AllStatistics)
-//    .AddJob(Job.Default.WithRuntime(CoreRuntime.Core50))
-//    .AddExporter(DefaultExporters.Markdown);
-
-//BenchmarkRunner.Run<SealedClassBenchmark>(customConfig);
