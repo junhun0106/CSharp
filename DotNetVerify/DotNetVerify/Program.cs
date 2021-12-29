6e1d372e799a22ac4a7e60549c1b8d9e6e96491b
kimjunghun <lapulass@naver.com>
2021-12-29 16:48:11 +0900

    봉인 된 속성 테스트 추가

--- DotNetVerify/DotNetVerify/Program.cs
+++ DotNetVerify/DotNetVerify/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿global using System;
+global using System.Reflection;
+global using BenchmarkDotNet.Attributes;
+
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+
+using DotNetVerify.SealedClass;
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+
+//var customConfig = ManualConfig
+//    .Create(DefaultConfig.Instance)
+//    .AddValidator(JitOptimizationsValidator.FailOnError)
+//    .AddDiagnoser(MemoryDiagnoser.Default)
+//    .AddColumn(StatisticColumn.AllStatistics)
+//    .AddJob(Job.Default.WithRuntime(CoreRuntime.Core50))
+//    .AddExporter(DefaultExporters.Markdown);
+
+//BenchmarkRunner.Run<SealedClassBenchmark>(customConfig);
---
+++ DotNetVerify/DotNetVerify/SealedClass/SealedClassBenchmark.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+
+namespace DotNetVerify.SealedClass
+{
+    [MemoryDiagnoser]
+    public class SealedAttributeBenchmark
+    {
+        class NoneSealedAttribute : Attribute
+        {
+        }
+
+        sealed class SealedAttribute : Attribute
+        {
+        }
+
+        [NoneSealed]
+        class CNoneSealedAttribute { }
+
+
+        [Sealed]
+        class CSealedAttribute { }
+
+        private readonly CNoneSealedAttribute _noneSealed = new CNoneSealedAttribute();
+        private readonly CSealedAttribute _sealed = new CSealedAttribute();
+
+        [Benchmark]
+        public void NoneSealed()
+        {
+            _ = _noneSealed.GetType().GetCustomAttribute<NoneSealedAttribute>();
+        }
+
+        [Benchmark]
+        public void Sealed()
+        {
+            _ = _sealed.GetType().GetCustomAttribute<SealedAttribute>();
+        }
+    }
+}
